ace33a9c5d9b9268d72eda1015ebf62334334e3f
dantinok9-bit <dantinok9@gmail.com>
2026-05-27 14:27:19 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "hd2-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon192.png",
+  "./icon512.png",
+  "./Randomizer_black.png",
+  "./Jumping_Diver.png",
+  "./Warbonds.png",
+  "./Factions.png",
+  "./Statistics.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.url.includes("script.google.com")) return;
+  if (e.request.url.includes("fonts.googleapis.com")) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
